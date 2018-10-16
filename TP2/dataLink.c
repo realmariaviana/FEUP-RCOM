23d07a91a4c4b2420598df831517b710f7949f13
simaosilva <simaosilva>
2018-10-16 23:54:09 +0100

    llopen and llclose done but not tested

--- TP2/dataLink.c
+++ TP2/dataLink.c
@@ -1,269 +1,317 @@
-/*Non-Canonical Input Processing*/
-
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <termios.h>
-#include <stdio.h>
-
-#define BAUDRATE B38400
-#define MODEMDEVICE "/dev/ttyS1"
-#define _POSIX_SOURCE 1 /* POSIX compliant source */
-#define FALSE 0
-#define TRUE 1
-#define FLAG 0x7E
-#define A_SEND 0x03
-#define C_SET 0x03
-#define SET_BCC 0x00
-#define C_UA 0x07
-#define C_DISC 0x0B
-#define TRANSMITTER 0
-#define RECEIVER 1
+#include "dataLink.h"
 
 char SET[5] = {FLAG, A_SEND, C_SET, A_SEND ^ C_SET, FLAG};
 char UA[5] = {FLAG, A_SEND, C_UA, A_SEND ^ C_UA, FLAG};
 char DISC[5] ={FLAG, A_SEND, C_DISC, A_SEND ^ C_DISC, FLAG};
-struct termios oldtio, newtio;
 
 volatile int STOP=FALSE;
+int timeOut = FALSE;
+int count = 0;
 
+void alarmHandler(int sig){
+  timeOut = true;
+  count ++;
+}
+
+int stateMachine(unsigned char c, int state, char * msg){
+  switch (state) {
+
+    case 0:
+      if(c == msg[0])
+        return 1;
+    break;
+
+    case 1:
+      if(c == msg[1])
+        return 2;
+
+      if(c != msg[0])
+        return 0;
+    break;
+
+    case 2:
+      if(c == msg[2])
+        return 3;
+
+      if(c!= msg[0])
+        return 0;
+      else
+        return 1;
+    break;
+
+    case 3:
+      if( c == (msg[2]^msg[1]))
+        return 4;
+
+      if(c!= msg[0])
+        return 0;
+      if( c== msg[0])
+        return 1;
+    break;
+
+    case 4:
+      if(c != msg[0])
+        return 0;
+      else
+        return 5;
+    break;
+
+    default:
+    break;
+  }
+  return 0;
+}
 
 int setTermios(int fd){
 
+    struct termios oldtio, newtio;
+
+    if ( tcgetattr(fd,&oldtio) == -1) { /* save current port settings */
+        perror("tcgetattr");
+        exit(-1);
+      }
+
+      bzero(&newtio, sizeof(newtio));
+      newtio.c_cflag = BAUDRATE | CS8 | CLOCAL | CREAD;
+      newtio.c_iflag = IGNPAR;
+      newtio.c_oflag = 0;
+
+      /* set input mode (non-canonical, no echo,...) */
+      newtio.c_lflag = 0;
+
+      newtio.c_cc[VTIME]    = 0;   /* inter-character timer unused */
+      newtio.c_cc[VMIN]     = 1;   /* blocking read until 5 chars received */
 
 
-if ( tcgetattr(fd,&oldtio) == -1) { /* save current port settings */
-      perror("tcgetattr");
-      exit(-1);
-    }
-
-    bzero(&newtio, sizeof(newtio));
-    newtio.c_cflag = BAUDRATE | CS8 | CLOCAL | CREAD;
-    newtio.c_iflag = IGNPAR;
-    newtio.c_oflag = 0;
-
-    /* set input mode (non-canonical, no echo,...) */
-    newtio.c_lflag = 0;
-
-    newtio.c_cc[VTIME]    = 0;   /* inter-character timer unused */
-    newtio.c_cc[VMIN]     = 1;   /* blocking read until 5 chars received */
+    /*
+      VTIME e VMIN devem ser alterados de forma a proteger com um temporizador a
+      leitura do(s) pr�ximo(s) caracter(es)
+    */
 
 
-  /*
-    VTIME e VMIN devem ser alterados de forma a proteger com um temporizador a
-    leitura do(s) pr�ximo(s) caracter(es)
-  */
+      tcflush(fd, TCIOFLUSH);
+
+      if ( tcsetattr(fd,TCSANOW,&newtio) == -1) {
+        perror("tcsetattr");
+        exit(-1);
+      }
+
+  	return 0;
+
+  }
+
+int llopen(int port, status mode){
+      int fd;
+
+      link_layer.mode = mode; //receiver or transmitter
+
+      switch (port) {
+        case COM1:
+        strcpy(link_layer.port, COM1_PORT);
+        break;
+
+        case COM2:
+        strcpy(link_layer.port, COM2_PORT);
+        break;
+
+        default:
+        printf("data_link - llopen(): invalid port!\n");
+        return -1;
+      }
+
+      fd = open(link_layer.port, O_RDWR | O_NOCTTY );
+      if (fd <0) {perror(link_layer.port); exit(-1); }
+
+      if (setTermios(fd) != 0) {
+        printf("dataLink - llopen() - setTermios: error\n");
+        return -1;
+      }
+
+      link_layer.mode = mode;
+
+      if(mode == TRANSMITTER)
+      if(llopenTransmitter(fd) <0)
+      return -1;
 
 
-    tcflush(fd, TCIOFLUSH);
+      if(mode == RECEIVER)
+      if(llopenReceiver(fd) < 0)
+      return -1;
 
-    if ( tcsetattr(fd,TCSANOW,&newtio) == -1) {
-      perror("tcsetattr");
-      exit(-1);
-    }
+      link_layer.sequenceNumber = 0;
 
-	return 0;
-
-}
-
-
+      return fd;
+  }
 
 int llopenTransmitter(int fd){
+  unsigned char c;
+  int state = 0;
 
- write(fd, SET, 5);
-	//RECEIVE UA
-    unsigned int state=0;
-	int i = 0;
-	int res;
-	sleep(1);
-	unsigned char buf[5];
+  signal(SIGALRM, alarmHandler);
 
+  do{
 
-    while (STOP==FALSE) {       /* loop for input */
-      res = read(fd,&buf,1);
-		
-     
-      switch (state) {
-        case 0:
-          if(buf[0] == FLAG){
-            state = 1;//flag received
-       }
-          else{
-            state = 0;
-		}
-          break;
-        case 1:
-          if(buf[0] == A_SEND)
-            state=2;//address received
-          else if(buf[0]==FLAG)
-            state=1;
-          else{
-            state=0;
-		}
-          break;
-        case 2:
-          if(buf[0]==C_UA){//control UA received
-            state=3;
-		
-}
-          else if(buf[0]==FLAG)
-            state=1;
-          else
-            state=0;
-break;
-        case 3:
-          if(buf[0]==(A_SEND^C_UA)){//bcc received
-            state=4;
-}
-          else if(buf[0]==FLAG)
-            state=1;
-          else
-            state=0;
-break;
-        case 4:
-          if(buf[0]==FLAG){//bcc received
-			STOP=TRUE;
-          }
-          else
-            state=0;
-break;
-      }
-i++;
+    if(write(fd, SET, 5) != 5){
+      printf("dataLink - llopen: error writting SET");
+      exit(-1);
     }
 
-    //printf("%c\n", buf[0]);
-    printf("UA received\n");
+    timeOut = false;
+    alarm(link_layer.timeout);
 
+    while(state != 5 && !timeOut){
 
-    return 0;
+      if(read(fd, &c, 1) == -1){
+        printf("dataLink - llopen: error reading");
+        exit(-1);
+      }
+        state = stateMachine(c, state, SET);
+    }
 
+  } while(timeOut && count < link_layer.transmissions);
 }
 
 int llopenReceiver(int fd){
- unsigned char c;
+  unsigned char c;
   int state=0;
 
   while(state!=5){
 
-    read(fd,&c,1);
+     if(read(fd, &c, 1) == -1){
+       printf("dataLink - llopen: read error");
+       exit(-1);
+     }
 
-    switch(state){
-      //recebe flag
-      case 0:
-	printf("0\n");
-        if(c==FLAG)
-          state=1;
-          break;
-      //recebe A
-      case 1:
-	printf("1\n");
-        if(c==A_SEND)
-          state=2;
-        else
-          {
-            if(c==FLAG)
-              state=1;
-            else
-              state = 0;
-          }
-      break;
-      //recebe C
-      case 2:
-	printf("2\n");
-        if(c==C_SET)
- 
-         state=3;
-        else{
-          if(c==FLAG)
-            state=1;
-          else
-            state = 0;
-        }
-      break;
-      //recebe BCC
-      case 3:
-	printf("3\n");
-        if(c==SET_BCC)
-          state = 4;
-        else
-          state=0;
-      break;
-      //recebe FLAG final
-      case 4:
-	printf("4\n");
-        if(c==FLAG){
-	  printf("Recebeu SET\n");
-    printf("%d\n", UA[0]);
-    printf("%d\n", UA[1]);
-    printf("%d\n", UA[2]);
-    printf("%d\n", UA[3]);
-    printf("%d\n", UA[4]);
-	  write(fd,UA,5);
-	  printf("Enviou UA\n");
-	  state=5;
-	}
-        else
-          state = 0;
-      break;
-		
-    } 
-  }
+     state = stateMachine(c, state, SET);
 
+   }
+
+   if(write(fd, UA, 5) != 5){
+     printf("dataLink - llopen: error writing UA");
+     exit(-1);
+   }
+
+   return 0;
 }
 
-int llopen(int porta, int mode){
+int llclose(int fd){
 
-	int fd;
-
-	
-  if(mode == 0)
-  if(llopenTransmitter(fd) <0)
+  if(link_layer.mode == TRANSMITTER)
+  if(llcloseTransmitter(fd) <0)
   return -1;
 
+  if(link_layer.mode == RECEIVER)
+  if(llcloseReceiver(fd) < 0)
+  return -1;
 
-  if(mode == 1)
-  if(llopenReceiver(fd) < 0)
-	return -1;
+  if ( tcsetattr(fd, TCSANOW, &link_layer.portSettings) == -1) {
+    perror("tcsetattr");
+    exit(-1);
+  }
 
-tcsetattr(fd,TCSANOW,&oldtio);
-    close(fd);
+  close(fd);
+  return 0;
+}
 
-	return 0;
+int llcloseTransmitter(int fd){
+  unsigned char c;
+  int state = 0;
+
+  signal(SIGALRM, alarmHandler);
+
+  do{
+
+    if(write(fd, DISC, 5) != 5){
+      printf("dataLink - llclose: error writting DISC");
+      return -1;
+    }
+
+    timeOut = false;
+    alarm(link_layer.timeout);
+
+    while(state != 5 && !timeOut){
+
+      if(read(fd, &c, 1) == -1){
+        printf("dataLink - llclose: error reading");
+        return -1;
+      }
+        state = stateMachine(c, state, DISC);
+    }
+
+  } while(timeOut && count < link_layer.transmissions);
+
+  if(write(fd, UA, 5) != 5){
+    printf("dataLink - llclose: error writting UA");
+  }
+  return 0;
+}
+
+int llcloseReceiver(int fd){
+  unsigned char c;
+  int state = 0;
+
+  while(state != 5){
+
+     if(read(fd, &c, 1) == -1){
+       printf("dataLink - llclose: error reading DISC");
+       return -1;
+     }
+
+     state = stateMachine(c, state, DISC);
+
+   }
+
+   if(write(fd, DISC, 5) != 5){
+     printf("dataLink - llclose: error writing DISC");
+     exit(-1);
+   }
+
+   state = 0;
+
+   while(state != 5) {
+     if(read(fd, &c, 1) == -1){
+       printf("dataLink - llclose: error reading UA");
+       return -1;
+     }
+
+     state = stateMachine(c, state, UA);
+  }
+   return 0;
 }
 
 int main(int argc, char** argv)
-{
+  {
 
-    int fd,c, res,mode;
-    struct termios oldtio,newtio;
-    unsigned char buf[5];
-    int i, sum = 0, speed = 0;
+      int fd,c, res,mode;
+      struct termios oldtio,newtio;
+      unsigned char buf[5];
+      int i, sum = 0, speed = 0;
 
-	 if ( (argc < 3) ||
-  	     ((strcmp("/dev/ttyS0", argv[1])!=0) &&
-  	      (strcmp("/dev/ttyS1", argv[1])!=0) )) {
-      printf("Usage:\tnserial SerialPort\n\tex: nserial /dev/ttyS1\n");
-      exit(1);
-	}
+  	 if ( (argc < 3) ||
+    	     ((strcmp("/dev/ttyS0", argv[1])!=0) &&
+    	      (strcmp("/dev/ttyS1", argv[1])!=0) )) {
+        printf("Usage:\tnserial SerialPort\n\tex: nserial /dev/ttyS1\n");
+        exit(1);
+  	}
 
-	 if(strcmp(argv[2],"T")==0){
-		printf("MODE: TRANSMITER\n");
-		mode=0;
-}
-	else if(strcmp(argv[2],"R")==0){
-	printf("MODE: RECEIVER\n");
-		mode=1;
-}
-	else
-	printf("Usage: T or R to transitter/receiver mode");
+  	 if(strcmp(argv[2],"T")==0){
+  		printf("MODE: TRANSMITER\n");
+  		mode=0;
+  }
+  	else if(strcmp(argv[2],"R")==0){
+  	printf("MODE: RECEIVER\n");
+  		mode=1;
+  }
+  	else
+  	printf("Usage: T or R to transitter/receiver mode");
 
 
-	   fd = open(argv[1], O_RDWR | O_NOCTTY );
-    if (fd <0) {perror(argv[1]); exit(-1); }
-	
-	setTermios(fd);
-	llopen(fd,mode);
+  	   fd = open(argv[1], O_RDWR | O_NOCTTY );
+      if (fd <0) {perror(argv[1]); exit(-1); }
 
-	return 0;
-}
+  	setTermios(fd);
+  	llopen(fd,mode);
+    llclose(fd);
 
+  	return 0;
+  }
